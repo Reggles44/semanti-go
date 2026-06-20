8dffc65a234d9c1edc9f4a2d15c08d77c6b7e9b9
Reggles <reginaldbeakes@gmail.com>
2026-06-20 17:17:54 -0400

    removing automatic service setup

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -16,38 +16,38 @@ download_binary() {
   mv semanti /usr/local/bin
 }
 
-setup_service() {
-  SERVICE="semanti.service"
-  SERVICE_FILE="/etc/systemd/system/$SERVICE"
-
-  # Create Service
-
-  # Remove the service file if it exists
-  #
-  if [ -f $SERVICE_FILE ]; then
-    rm $SERVICE_FILE
-  fi
-
-  cat >/etc/systemd/system/"$SERVICE" <<EOL
-[Unit]
-Description=Semanti GO
-After=network.target
-[Service]
-Type=simple
-ExecStart=/usr/local/bin/semanti
-[Install]
-WantedBy=multi-user.target
-EOL
-
-  sudo systemctl enable $SERVICE
-  sudo systemctl start $SERVICE
-
-  if systemctl -q is-active $SERVICE; then
-    echo "$SERVICE is up and running!"
-  else
-    echo "Failed to start $SERVICE."
-  fi
-}
+# setup_service() {
+#   SERVICE="semanti.service"
+#   SERVICE_FILE="/etc/systemd/system/$SERVICE"
+#
+#   # Create Service
+#
+#   # Remove the service file if it exists
+#   #
+#   if [ -f $SERVICE_FILE ]; then
+#     rm $SERVICE_FILE
+#   fi
+#
+#   cat >/etc/systemd/system/"$SERVICE" <<EOL
+# [Unit]
+# Description=Semanti GO
+# After=network.target
+# [Service]
+# Type=simple
+# ExecStart=/usr/local/bin/semanti
+# [Install]
+# WantedBy=multi-user.target
+# EOL
+#
+#   sudo systemctl enable $SERVICE
+#   sudo systemctl start $SERVICE
+#
+#   if systemctl -q is-active $SERVICE; then
+#     echo "$SERVICE is up and running!"
+#   else
+#     echo "Failed to start $SERVICE."
+#   fi
+# }
 
 download_binary
-setup_service
+# setup_service
